a1bcac84cdafaa394ba78de15993418d9a271e70
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-25 01:39:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  piglist = {}
+  data.each do |category, info|
+    info.each do |subinfo, names|
+      names.each do |name|
+        if piglist.keys.include?(name)
+          if piglist[name][category]
+            piglist[name][category] << subinfo
+          else
+            piglist[name][category] = subinfo
+          end
+        else
+          piglist[name][category] = subinfo
+        end
+      end
+    end
+  end
+  piglist
 end
